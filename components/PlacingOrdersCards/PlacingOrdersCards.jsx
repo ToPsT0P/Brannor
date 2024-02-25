2917acdb3393724e4ac13eb6b633c2e445f96cf5
ToPsT0P <pustovalov.al.an@gmail.com>
2024-02-25 11:09:21 +0300

    Накинул стили и часть логики на выбор способа оплаты

--- components/PlacingOrdersCards/PlacingOrdersCards.jsx
+++ components/PlacingOrdersCards/PlacingOrdersCards.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import choosedIMG from "../../public/choosedIMG.png"
 
 const PlacingOrdersCards = () => {
 
@@ -8,24 +9,39 @@ const PlacingOrdersCards = () => {
                 <div className="placingOrder_rowingCards">
                     <div className="placingOrder_mainBlock">
                         <div className="placingOrder_deliveryType activeDeliveryCard">
-                            <h5>Доставка Курьером по Москве</h5>
+                            <div className="placingOrder_deliveryType_in">
+                                <h5>Доставка Курьером по Москве</h5>
+                                <img src={choosedIMG} alt=""/>
+                            </div>
                             <p>Бесплатно / до 3 дней</p>
                         </div>
                         <div className="placingOrder_deliveryType">
-                            <h5>Доставка ТК СДЭК</h5>
+                            <div className="placingOrder_deliveryType_in">
+                                <h5>Доставка ТК СДЭК</h5>
+                                <img src={choosedIMG} alt=""/>
+                            </div>
                             <p>от 500 ₽ / до 2 дней  </p>
                         </div>
                         <div style={{marginTop:"14px"}} className="placingOrder_deliveryType">
-                            <h5>Доставка ТК ПЭК</h5>
+                            <div className="placingOrder_deliveryType_in">
+                                <h5>Доставка ТК ПЭК</h5>
+                                <img src={choosedIMG} alt=""/>
+                            </div>
                             <p>от 350 ₽ / до 6 дней  </p>
                         </div>
                         <div style={{marginTop:"14px"}} className="placingOrder_deliveryType">
-                            <h5>Другие ТК</h5>
+                            <div className="placingOrder_deliveryType_in">
+                                <h5>Другие ТК</h5>
+                                <img src={choosedIMG} alt=""/>
+                            </div>
                             <p>500 ₽ / до 6 дней  </p>
                         </div>
                     </div>
                     <div className="placingOrder_deliveryType fullheight">
-                        <h5>Самовывоз</h5>
+                        <div className="placingOrder_deliveryType_in">
+                            <h5>Самовывоз</h5>
+                            <img src={choosedIMG} alt=""/>
+                        </div>
                         <p>Бесплатно / до 3 дней</p>
                     </div>
 
--- pages/CartPage/CartPage.jsx
+++ pages/CartPage/CartPage.jsx
@@ -1,9 +1,20 @@
 import React, { useEffect, useState } from "react";
 import CartItem from "../../components/CartItem/CartItem";
+import cartList_inputIMG from "../../public/CartList_inputIMG.png"
 
 const CartPage = () => {
 
+    const promo = ["123qwe","asdqwe"]
+    const [userPromo, setUserPromo] = useState("")
+    const [promoAnswer, setPromoAnswer] = useState(false)
     const [dataArray, setDataArray] = useState([
+        {
+            id: 1,
+            imageURL: "rotor.png",
+            name: "shit not shit ",
+            price: "41000",
+            quantity: 1,
+        },
         {
             id: 1,
             imageURL: "rotor.png",
@@ -20,6 +31,20 @@ const CartPage = () => {
         }
 
     ])
+
+    const checkPromo = () => {
+        let i = 0
+        while(i < promo.length){
+            if(userPromo == promo[i]){
+                setPromoAnswer(true)
+                return true
+            }
+            else{
+                i++
+            }
+        }
+
+    }
     
     const deleteArrayObj = (props) => {
         setDataArray(dataArray.filter(p => p !== props))
@@ -41,6 +66,10 @@ const CartPage = () => {
                      deleteArrayObj={deleteArrayObj}/>)})}
                      {dataArray.length == 0 && <h2>Товаров в корзине нет</h2>}
 
+                    <div className="cartList_input">
+                        <input placeholder="Введите промокод" onChange={(e) => {setUserPromo(e.target.value)}}/>
+                        <img src={cartList_inputIMG} alt="" onClick={() => {checkPromo()}}/>
+                    </div>
                 </div>
                 <div className="cartDetails">
                     <h2>Детали заказа</h2>
@@ -55,21 +84,21 @@ const CartPage = () => {
                         <p>скидка</p>
                         <div className="dottedLine"></div>
                         <div className="orderDetailsCounters">
-                            0Р
+                           0₽ 
                         </div>
                     </div>
                     <div className="orderDetailsAll">
                         <p>промо</p>
                         <div className="dottedLine"></div>
                         <div className="orderDetailsCounters">
-                            ASS21
+                            {promoAnswer ? <p>500₽</p> : <p>0₽</p>  } 
                         </div>
                     </div>
                     <div className="orderDetailsAll">
                         <b>Итого</b>
                         <div className="dottedLine"></div>
                         <div className="orderDetailsCounters">
-                            123213
+                            1232₽ 
                         </div>
                     </div>
                     <button onClick={() => {window.location.assign("/profile/PlacingOrder")}}>Оформить заказ</button>
